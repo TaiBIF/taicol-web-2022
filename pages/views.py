c3d9644fc5a7e9dae2619c87fad06cede43bf7a2
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2022-07-15 16:38:53 +0800

    about & policy page

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.utils import timezone
+from datetime import datetime, timedelta
+
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+
+def policy(request):
+    now = timezone.now() + timedelta(hours=8)
+    today = now.strftime("%Y-%m-%d")
+    return render(request, 'pages/data_policy.html', {'today': today})
 
 
 def index(request):
