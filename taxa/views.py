cb1c5fcdfb51b60edf2395d8564fbdc3dc56ba03
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2022-07-27 15:39:10 +0800

    option to filter

--- taxa/views.py
+++ taxa/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from taxa.utils import rank_map, rank_map_c
 
 
 def catalogue(request):
     # 0 不開 1 開一層 2 全開
-    option = request.GET.get('option', 0)
-    return render(request, 'taxa/catalogue.html', {'option': option})
+    filter = request.GET.get('filter', 0)
+    return render(request, 'taxa/catalogue.html', {'filter': filter, 'ranks': rank_map_c})
 
 
 def name_match(request):
