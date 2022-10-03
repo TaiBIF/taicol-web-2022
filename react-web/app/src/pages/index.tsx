655924b8e3f6d0b19881de24c1deab790a62c770
Terry Yip <terry.hk796@gmail.com>
2022-10-03 16:41:29 +0800

    add scroll trigger function in homepage

--- react-web/app/src/pages/index.tsx
+++ react-web/app/src/pages/index.tsx
@@ -1,13 +1,32 @@
 import TaxonCountSection from 'src/components/frontend/home/TaxonCountSection'
 import LatestNewsListSection from 'src/components/frontend/home/LatestNewsListSection'
 import Banner from 'src/components/frontend/home/Banner'
-import React from 'react'
-
+import React, { useRef,useEffect } from 'react'
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 const Home = () => {
+
+  useEffect(() => {
+
+    ScrollTrigger.create({
+      trigger: '.section-2-statistics',
+			start: "top-=60% top",
+      toggleClass: "vivi"
+    });
+
+    ScrollTrigger.create({
+      trigger: '.section-3-news',
+			start: "top-=40% top",
+      toggleClass: "vivi"
+    });
+  }, [])
+
+
   return (
     <div className="ovh">
       <Banner/>
-      <TaxonCountSection />
+      <TaxonCountSection   />
       <LatestNewsListSection/>
     </div>
   )
