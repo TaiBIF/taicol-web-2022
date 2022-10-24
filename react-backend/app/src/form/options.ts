ec14ed6a4afc593539e4d6b80cfc05d422de401a
Terry Yip <terry.hk796@gmail.com>
2022-10-24 12:41:53 +0800

    added xlsx ,docx for dowload type

--- react-backend/app/src/form/options.ts
+++ react-backend/app/src/form/options.ts
@@ -1,7 +1,7 @@
 import type { OptionProp } from 'src/types';
 
 export const roles = ['admin', 'user'] as const;
-export const filetypes = ['csv', 'pdf','doc','xls'] as const;
+export const filetypes = ['csv', 'pdf','doc','xls','xlsx','docx'] as const;
 export const categoryTypes = ['news', 'article','download'] as const;
 
 export type Role = typeof roles[number];
@@ -23,15 +23,17 @@ export type CategoryOption = OptionProp & {
 export const RoleOptions: RoleOption[] = [
 	{
 		label: 'Administrator',
-		value: 'admin',
+		value: 'admin'
 	},
 ];
 
 export const DownloadFileTypeOptions: DownloadFileTypeOption[] = [
-	{ label: 'CSV',value: 'csv',},
-	{ label: 'PDF',value: 'pdf',},
-	{ label: 'XLS',value: 'xls',},
-	{ label: 'DOC',value: 'doc',},
+	{ label: 'CSV',value: 'csv'},
+	{ label: 'PDF',value: 'pdf'},
+	{ label: 'XLS',value: 'xls'},
+	{ label: 'XLSX',value: 'xlsx'},
+	{ label: 'DOC',value: 'doc'},
+	{ label: 'DOCX',value: 'docx'},
 ];
 
 export const CategoryTypeOptions: CategoryOption[] = [
