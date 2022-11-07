635f696a2c121dbe706dba3dacd4423e361b5ed4
Terry Yip <terry.hk796@gmail.com>
2022-11-07 14:58:32 +0800

    fixed 臺灣各類生物種數與特有比例數字

--- react-backend/app/src/pages/statistics/index.tsx
+++ react-backend/app/src/pages/statistics/index.tsx
@@ -117,8 +117,8 @@ const Statistics: React.VFC = () => {
   const formatEndemicCountData = (props: (string | number)[][]): void => {
       const endemicData:EndemicProps[] = props.map((item: (string | number)[]):EndemicProps => {
         const image = endemicInfo.find((r) => r.endemic === item[0])?.image || ''
-        const name = item[1] as string
-        const count = item[1] as number
+        const name = item[0] as string
+        const count = item[2] as number
         const total = item[2] as number
 
         return {
