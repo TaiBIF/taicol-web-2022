4d98cde287de6074696e9f4173b0ec354a4a3531
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2023-11-06 10:20:41 +0800

    update search filter

--- static/js/taxon.js
+++ static/js/taxon.js
@@ -161,7 +161,7 @@ $(function(){
 			$lang == 'en-us' ? alert("Please enter keywords") : alert("請輸入關鍵字");
 
 		} else {
-			window.location = '/catalogue?keyword=' + $('input[name=keyword]').val()
+			window.location = '/catalogue?filter=0&keyword=' + $('input[name=keyword]').val()
 		}
 	})
 	// 收和控制
